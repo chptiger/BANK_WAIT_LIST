abea294331882a5c21ac3ec4690d7e822a7270c1
xx65 <thomas.java863@gmail.com>
2018-08-26 14:43:06 -0700

    to fixe null exception

--- src/com/xx/bank/MainClass.java
+++ src/com/xx/bank/MainClass.java
@@ -3,19 +3,19 @@ package com.xx.bank;
 public class MainClass {
 
 	public static void main(String[] args) {
-//		common service window: 4
-		for(int i = 1; i<5; i++) {
+		// common service window: 4
+		for (int i = 1; i < 5; i++) {
 			ServiceWindow commmonService = new ServiceWindow();
 			commmonService.start();
 			commmonService.setWindowId(i);
 		}
-		
-//	express window: 1
+
+		// express window: 1
 		ServiceWindow expressService = new ServiceWindow();
 		expressService.setType(CustomerType.EXPRESS);
 		expressService.start();
-		
-//		vip window: 1
+
+		// vip window: 1
 		ServiceWindow vipService = new ServiceWindow();
 		vipService.setType(CustomerType.VIP);
 		vipService.start();
